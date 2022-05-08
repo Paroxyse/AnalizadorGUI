ec89b6cd0b545dba8244b8e6f058da92f0e125d3
Daniel Montoya <Spacesloth@outlook.es>
2022-05-07 22:00:49 -0500

    Ajustar métodos a GUI Probar con Funciones y conjuntos anteriores

--- AnalizadorGUI/MyForm.cpp
+++ AnalizadorGUI/MyForm.cpp
@@ -36,227 +36,227 @@ void main(array<String^>^ args)
 
 
 
-using namespace std;
-string LeerArc(string filetl) {
-    ifstream file;
-    file.open(filetl, ios::in);
-
-    if (file.fail())
-    {
-        cout << "El archivo " << filetl << " no existe" << endl;
-        return "";
-    }
-    string str;
-    char c;
-
-    while (file.get(c))
-    {
-        str.push_back(c);
-    }
-    file.close();
-
-    return str;
-}
-void loadSets() {
-    //Código que podría reemplazar por algo más flexible, pero dejaré ese cambio para el analizador completo
-    mayus = LeerArc("Mayus.txt");
-    minun = LeerArc("Minus.txt");
-    input = LeerArc("Entrada.txt");
-}
-int isLetter(char c)
-{
-    for (int i = 0; i < mayus.size(); i++)
-    {
-        if (c == mayus.at(i))
-        {
-            return 1;
-        }
-        if (c == minun.at(i))
-        {
-            return 2;
-        }
-    }
-    return 0;
-}
-int isSomethingelse(char c, string str) {
-    string s = LeerArc(str);
-    for (int i = 0; i < s.size(); i++) {
-        if (c == s.at(i))
-        {
-            return i + 5;
-        }
-    }
-    return -1;
-}
-int dataType(char c, string s)
-{
-    if (c >= '0' && c <= '9')
-    {
-        return 0;
-    }
-    int aux = isLetter(c);
-    if (aux >= 1 && aux <= 2)
-    {
-        //cout << aux;
-        return aux;
-    }
-    // code some return shenanigans to fix the e thing
-    // Arreglar lo de la e en versión completa
-    if (c == '\n')
-    {
-        return 3;
-    }
-    if (c == '\t' || c == ' ')
-    {
-        return 4;
-    }
-    aux = isSomethingelse(c, s);
-    if (aux != 0)
-    {
-        return aux;
-    }
-    return -1;
-}
-void cargarFT(string s)
-{
-    //Gracias a la maravillosa persona de SO que hizo esto posible
-    //Esta funcion llena un vector de enteros bidimensional con la información contenida en un archivo
-
-    std::ifstream sourceFileStream{ s };
-
-    if (sourceFileStream) {
-        std::string line{};
-        while (std::getline(sourceFileStream, line))
-        {
-            FT.push_back(std::vector<int>{});
-            int c{};
-            for (std::istringstream iss(line); iss >> c; FT.back().push_back(c))
-                ;
-        }
-
-        for (const auto& row : FT)
-        {
-            for (const auto& col : row) std::cout << col << ' ';
-            std::cout << '\n';
-        }
-    }
-    else std::cerr << "\nError: No se encontró el archivo\n\n";
-}
-vector<int> cargarVint(string s)
-{
-    vector<int> aux{};
-    ifstream input(s);
-    if (input)
-    {
-        int valor;
-        while (input >> valor)
-        {
-            aux.push_back(valor);
-        }
-        return aux;
-    }
-    cout << "Archivo conteniendo una lista de enteros no encontrado";
-    return aux;
-}
-vector<string> cargarVstring(string s)
-{
-    vector<string> aux{};
-    ifstream input;
-    input.open(s);
-    if (input)
-    {
-        string cadena;
-        while (input >> cadena)
-        {
-            aux.push_back(cadena);
-        }
-    }
-    else
-    {
-        cout << "Archivo conteniendo una lista de strings no encontrado";
-    }
-    return aux;
-}
-int getItemIndex(int code, vector<int> v)
-{
-    if (v.size() == 0)
-    {
-        return -1;
-    }
-    for (int i = 0; i < v.size(); i++)
-    {
-        if (code == v[i])
-        {
-            return i;
-        }
-    }
-
-}
-void analizar(string inputString, string charset, string TFunc, string CodeList, string MessageList, bool InputMode)
-{
-
-    cargarFT(TFunc);
-    int state = 0;
-    int symb;
-    int i = 0;
-    string inString;
-    output = "";
-    inString = LeerArc(inputString);
-    while (i < inString.size())
-    {
-        symb = dataType(inString.at(i), charset);
-        cout << "\n";
-        if (symb == -1) {
-            state = 500;
-            cout << i << " " << symb << " " << state << " " << inString.at(i);
-        }
-        if (state < TFunc.size())
-        {
-
-
-            state = FT[state][symb];
-
-
-            cout << i << " " << symb << " " << state << " " << inString.at(i);
-
-            //Si este es el último caracter o si el siguiente estado desde el actual explota, corta el token aquí y salta a un estado
-             //de final1
-            if (i == inString.size() - 1 && state < FT.size())
-            {
-                state = FT[state][FT[state].size() - 1];
-            }
-            //else if(FT[state][dataType(inString.at(i+1), charset)]>TFunc.size())
-            //{
-            //    
-            //    //state = FT[state][FT[state].size() - 1];
-            //}
-
-        }
-
-        if (state > TFunc.size())
-        {
-            //load code list
-            vector<int> cList = cargarVint(CodeList);
-            //load message list
-            vector<string> mList = cargarVstring(MessageList);
-            //Get index on the message list from the code list
-            int ItemIndex = getItemIndex(state, cList);
-            if (ItemIndex == -1)
-            {
-                ItemIndex = getItemIndex(600, cList);
-            }
-            output.append(mList.at(ItemIndex));
-            output.append("\n");
-
-            state = 0;
-        }
-
-
-
-
-
-        i++;
-    }
-    cout << "\n" << output;
-
-}
+//using namespace std;
+//string LeerArc(string filetl) {
+//    ifstream file;
+//    file.open(filetl, ios::in);
+//
+//    if (file.fail())
+//    {
+//        cout << "El archivo " << filetl << " no existe" << endl;
+//        return "";
+//    }
+//    string str;
+//    char c;
+//
+//    while (file.get(c))
+//    {
+//        str.push_back(c);
+//    }
+//    file.close();
+//
+//    return str;
+//}
+//void loadSets() {
+//    //Código que podría reemplazar por algo más flexible, pero dejaré ese cambio para el analizador completo
+//    mayus = LeerArc("Mayus.txt");
+//    minun = LeerArc("Minus.txt");
+//    input = LeerArc("Entrada.txt");
+//}
+//int isLetter(char c)
+//{
+//    for (int i = 0; i < mayus.size(); i++)
+//    {
+//        if (c == mayus.at(i))
+//        {
+//            return 1;
+//        }
+//        if (c == minun.at(i))
+//        {
+//            return 2;
+//        }
+//    }
+//    return 0;
+//}
+//int isSomethingelse(char c, string str) {
+//    string s = LeerArc(str);
+//    for (int i = 0; i < s.size(); i++) {
+//        if (c == s.at(i))
+//        {
+//            return i + 5;
+//        }
+//    }
+//    return -1;
+//}
+//int dataType(char c, string s)
+//{
+//    if (c >= '0' && c <= '9')
+//    {
+//        return 0;
+//    }
+//    int aux = isLetter(c);
+//    if (aux >= 1 && aux <= 2)
+//    {
+//        //cout << aux;
+//        return aux;
+//    }
+//    // code some return shenanigans to fix the e thing
+//    // Arreglar lo de la e en versión completa
+//    if (c == '\n')
+//    {
+//        return 3;
+//    }
+//    if (c == '\t' || c == ' ')
+//    {
+//        return 4;
+//    }
+//    aux = isSomethingelse(c, s);
+//    if (aux != 0)
+//    {
+//        return aux;
+//    }
+//    return -1;
+//}
+//void cargarFT(string s)
+//{
+//    //Gracias a la maravillosa persona de SO que hizo esto posible
+//    //Esta funcion llena un vector de enteros bidimensional con la información contenida en un archivo
+//
+//    std::ifstream sourceFileStream{ s };
+//
+//    if (sourceFileStream) {
+//        std::string line{};
+//        while (std::getline(sourceFileStream, line))
+//        {
+//            FT.push_back(std::vector<int>{});
+//            int c{};
+//            for (std::istringstream iss(line); iss >> c; FT.back().push_back(c))
+//                ;
+//        }
+//
+//        for (const auto& row : FT)
+//        {
+//            for (const auto& col : row) std::cout << col << ' ';
+//            std::cout << '\n';
+//        }
+//    }
+//    else std::cerr << "\nError: No se encontró el archivo\n\n";
+//}
+//vector<int> cargarVint(string s)
+//{
+//    vector<int> aux{};
+//    ifstream input(s);
+//    if (input)
+//    {
+//        int valor;
+//        while (input >> valor)
+//        {
+//            aux.push_back(valor);
+//        }
+//        return aux;
+//    }
+//    cout << "Archivo conteniendo una lista de enteros no encontrado";
+//    return aux;
+//}
+//vector<string> cargarVstring(string s)
+//{
+//    vector<string> aux{};
+//    ifstream input;
+//    input.open(s);
+//    if (input)
+//    {
+//        string cadena;
+//        while (input >> cadena)
+//        {
+//            aux.push_back(cadena);
+//        }
+//    }
+//    else
+//    {
+//        cout << "Archivo conteniendo una lista de strings no encontrado";
+//    }
+//    return aux;
+//}
+//int getItemIndex(int code, vector<int> v)
+//{
+//    if (v.size() == 0)
+//    {
+//        return -1;
+//    }
+//    for (int i = 0; i < v.size(); i++)
+//    {
+//        if (code == v[i])
+//        {
+//            return i;
+//        }
+//    }
+//
+//}
+//void analizar(string inputString, string charset, string TFunc, string CodeList, string MessageList, bool InputMode)
+//{
+//
+//    cargarFT(TFunc);
+//    int state = 0;
+//    int symb;
+//    int i = 0;
+//    string inString;
+//    output = "";
+//    inString = LeerArc(inputString);
+//    while (i < inString.size())
+//    {
+//        symb = dataType(inString.at(i), charset);
+//        cout << "\n";
+//        if (symb == -1) {
+//            state = 500;
+//            cout << i << " " << symb << " " << state << " " << inString.at(i);
+//        }
+//        if (state < TFunc.size())
+//        {
+//
+//
+//            state = FT[state][symb];
+//
+//
+//            cout << i << " " << symb << " " << state << " " << inString.at(i);
+//
+//            //Si este es el último caracter o si el siguiente estado desde el actual explota, corta el token aquí y salta a un estado
+//             //de final1
+//            if (i == inString.size() - 1 && state < FT.size())
+//            {
+//                state = FT[state][FT[state].size() - 1];
+//            }
+//            //else if(FT[state][dataType(inString.at(i+1), charset)]>TFunc.size())
+//            //{
+//            //    
+//            //    //state = FT[state][FT[state].size() - 1];
+//            //}
+//
+//        }
+//
+//        if (state > TFunc.size())
+//        {
+//            //load code list
+//            vector<int> cList = cargarVint(CodeList);
+//            //load message list
+//            vector<string> mList = cargarVstring(MessageList);
+//            //Get index on the message list from the code list
+//            int ItemIndex = getItemIndex(state, cList);
+//            if (ItemIndex == -1)
+//            {
+//                ItemIndex = getItemIndex(600, cList);
+//            }
+//            output.append(mList.at(ItemIndex));
+//            output.append("\n");
+//
+//            state = 0;
+//        }
+//
+//
+//
+//
+//
+//        i++;
+//    }
+//    cout << "\n" << output;
+//
+//}
--- AnalizadorGUI/MyForm.h
+++ AnalizadorGUI/MyForm.h
@@ -47,11 +47,22 @@ namespace AnalizadorGUI {
 	private: System::Windows::Forms::Button^ button3;
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::RichTextBox^ richTextBox3;
-	private: System::Windows::Forms::RichTextBox^ richTextBox2;
-	private: System::Windows::Forms::RichTextBox^ richTextBox1;
-	String ^FilePath;
-	
+	private: System::Windows::Forms::RichTextBox^ TBError;
+
+	private: System::Windows::Forms::RichTextBox^ TBToken;
+	private: System::Windows::Forms::RichTextBox^ TBFile;
+
+
+	String ^CharsetName="symbnum.txt";
+	String ^MayusName="Mayus.txt";
+	String^ MinusName = "Minus.txt";
+	String^ FTName = "TablaT0.txt";
+	String^ CLName = "ListaCodigos0.txt";
+	String^ MLName = "ListaMensajes0.txt";
+	String^ Input = "Entrada.txt";
+	private: System::Windows::Forms::RichTextBox^ TBProceso;
+
+	private: System::Windows::Forms::Label^ label5;
 	private:
 		/// <summary>
 		/// Required designer variable.
@@ -73,9 +84,11 @@ namespace AnalizadorGUI {
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->richTextBox3 = (gcnew System::Windows::Forms::RichTextBox());
-			this->richTextBox2 = (gcnew System::Windows::Forms::RichTextBox());
-			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->TBError = (gcnew System::Windows::Forms::RichTextBox());
+			this->TBToken = (gcnew System::Windows::Forms::RichTextBox());
+			this->TBFile = (gcnew System::Windows::Forms::RichTextBox());
+			this->TBProceso = (gcnew System::Windows::Forms::RichTextBox());
+			this->label5 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// label4
@@ -124,7 +137,7 @@ namespace AnalizadorGUI {
 			// 
 			// button4
 			// 
-			this->button4->Location = System::Drawing::Point(216, 585);
+			this->button4->Location = System::Drawing::Point(43, 581);
 			this->button4->Name = L"button4";
 			this->button4->Size = System::Drawing::Size(100, 40);
 			this->button4->TabIndex = 17;
@@ -134,7 +147,7 @@ namespace AnalizadorGUI {
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(43, 585);
+			this->button3->Location = System::Drawing::Point(366, 513);
 			this->button3->Name = L"button3";
 			this->button3->Size = System::Drawing::Size(100, 40);
 			this->button3->TabIndex = 16;
@@ -162,35 +175,56 @@ namespace AnalizadorGUI {
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
-			// richTextBox3
+			// TBError
 			// 
-			this->richTextBox3->Location = System::Drawing::Point(368, 343);
-			this->richTextBox3->Name = L"richTextBox3";
-			this->richTextBox3->Size = System::Drawing::Size(280, 150);
-			this->richTextBox3->TabIndex = 13;
-			this->richTextBox3->Text = L"";
+			this->TBError->Location = System::Drawing::Point(368, 343);
+			this->TBError->Name = L"TBError";
+			this->TBError->Size = System::Drawing::Size(280, 150);
+			this->TBError->TabIndex = 13;
+			this->TBError->Text = L"";
 			// 
-			// richTextBox2
+			// TBToken
 			// 
-			this->richTextBox2->Location = System::Drawing::Point(368, 124);
-			this->richTextBox2->Name = L"richTextBox2";
-			this->richTextBox2->Size = System::Drawing::Size(280, 150);
-			this->richTextBox2->TabIndex = 12;
-			this->richTextBox2->Text = L"";
+			this->TBToken->Location = System::Drawing::Point(368, 124);
+			this->TBToken->Name = L"TBToken";
+			this->TBToken->Size = System::Drawing::Size(280, 150);
+			this->TBToken->TabIndex = 12;
+			this->TBToken->Text = L"";
 			// 
-			// richTextBox1
+			// TBFile
 			// 
-			this->richTextBox1->Location = System::Drawing::Point(43, 124);
-			this->richTextBox1->Name = L"richTextBox1";
-			this->richTextBox1->Size = System::Drawing::Size(273, 369);
-			this->richTextBox1->TabIndex = 11;
-			this->richTextBox1->Text = L"";
+			this->TBFile->Location = System::Drawing::Point(43, 124);
+			this->TBFile->Name = L"TBFile";
+			this->TBFile->Size = System::Drawing::Size(273, 369);
+			this->TBFile->TabIndex = 11;
+			this->TBFile->Text = L"";
+			// 
+			// TBProceso
+			// 
+			this->TBProceso->Location = System::Drawing::Point(713, 124);
+			this->TBProceso->Name = L"TBProceso";
+			this->TBProceso->Size = System::Drawing::Size(273, 369);
+			this->TBProceso->TabIndex = 22;
+			this->TBProceso->Text = L"";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Digital-7", 24, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label5->Location = System::Drawing::Point(717, 76);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(265, 33);
+			this->label5->TabIndex = 23;
+			this->label5->Text = L"Proceso de analisis";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(684, 661);
+			this->ClientSize = System::Drawing::Size(1048, 661);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->TBProceso);
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label2);
@@ -199,9 +233,9 @@ namespace AnalizadorGUI {
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
-			this->Controls->Add(this->richTextBox3);
-			this->Controls->Add(this->richTextBox2);
-			this->Controls->Add(this->richTextBox1);
+			this->Controls->Add(this->TBError);
+			this->Controls->Add(this->TBToken);
+			this->Controls->Add(this->TBFile);
 			this->Name = L"MyForm";
 			this->Text = L"Analizador gamer";
 			this->ResumeLayout(false);
@@ -210,20 +244,20 @@ namespace AnalizadorGUI {
 		}
 
 #pragma endregion
-		//Convertir de string manejada a std
-		std::string convertirMU(System::String ^s) 
+	//Convertir de string manejada a std
+	std::string convertirMU(System::String ^s) 
 		{
 			std::string st = msclr::interop::marshal_as<std::string>(s); 
 				return st;
 		}
-		//Convertir de string std a manejada
-		System::String^ convertirUM(std::string s)
+	//Convertir de string std a manejada
+	System::String^ convertirUM(std::string s)
 		{
 			String^ st = gcnew String(s.c_str());
 			return st;
 		}
-		//Cargar Archivo a una string
-		std::string LeerArc(std::string filetl) {
+	//Cargar Archivo a una string
+	std::string LeerArc(std::string filetl) {
 			std::ifstream file;
 			file.open(filetl, std::ios::in);
 
@@ -243,16 +277,233 @@ namespace AnalizadorGUI {
 
 			return str;
 		}
+	//Verifica si un caracter es número
+	int isNumber(char c)
+	{
+		if (c >= '0' && c <= '9') 
+		{
+			return 0;
+		}
+		return -1;
+	}
+	//Carga archivos con el alfabeto y verifica si un caracter es letra
+	int isLetter(char c) 
+	{
+		std::string mayus = "";
+		std::string min = "";
+		mayus = LeerArc(convertirMU(MayusName));
+		min = LeerArc(convertirMU(MinusName));
+		//Se podría poner todo dentro del mismo for, pero eso asume que el conjunto mayus y min son del mismo tamaño
+		//Lo cual debería ser el caso, pero podría no serlo
+		for (int i = 0; i < mayus.size(); i++)
+		{
+			if (c == mayus.at(i))
+			{
+				return 1;
+			}
+		}
+		for (int i = 0; i < min.size(); i++)
+		{
+			if (c == min.at(i))
+			{
+				return 2;
+			}
+		}
+		return -1;
+	}
+	//Se manda una cadena que contiene todos los caracteres que no son parte de un conjunto y se retorna si el caracter está contenido
+	//en la cadena y en qué posición
+	int isSomethingElse(char c, std::string charsetfile)
+	{
+		std::string s = LeerArc(charsetfile);
+		for (int i = 0; i < s.size(); i++) {
+			if (c == s.at(i))
+			{
+				return i + 3;
+			}
+		}
+		return -1;
+	}
+	//Verifica con ayuda de los tres métodos anteriores el tipo de dato que tiene un caracter, si un caracter dentro de un conjunto
+	//tiene un comportamiento especial, retorna el comportamiento especial, en lugar de el de el conjunto (E/e en constantes mat)
+	int dataType(char c, std::string charsetfile)
+	{
+		int ret = -1;
+		int aux;
+		aux = isNumber(c);
+		if(aux!=-1)
+		{
+			ret = aux;
+		}
+		aux = isLetter(c);
+		if (aux != -1)
+		{
+			ret = aux;
+		}
+		aux = isSomethingElse(c, charsetfile);
+		if (aux != -1)
+		{
+			ret = aux;
+		}
+		return ret;
+	}
+	//Carga y Retorna una función de transición
+	std::vector<std::vector<int>> cargarFT(std::string filename)
+	{
+		std::vector<std::vector<int>> FT{};
+		std::ifstream sourceFileStream{ filename };
+
+		if (sourceFileStream) {
+			std::string line{};
+			while (std::getline(sourceFileStream, line))
+			{
+				FT.push_back(std::vector<int>{});
+				int c{};
+				for (std::istringstream iss(line); iss >> c; FT.back().push_back(c))
+					;
+			}
+
+			for (const auto& row : FT)
+			{
+				for (const auto& col : row) std::cout << col << ' ';
+				std::cout << '\n';
+			}
+		}
+		else std::cerr << "\nError: No se encontró el archivo\n\n";
+		return FT;
+	}
+	//Carga y retorna un vector de ints
+	std::vector<int> cargarVint(std::string s)
+	{
+		std::vector<int> aux{};
+		std::ifstream input(s);
+		if (input)
+		{
+			int valor;
+			while (input >> valor)
+			{
+				aux.push_back(valor);
+			}
+			return aux;
+		}
+		//cout << "Archivo conteniendo una lista de enteros no encontrado";
+		return aux;
+	}
+	//Carga y retorna un vector de strings
+	std::vector<std::string> cargarVstring(std::string s)
+	{
+		std::vector<std::string> aux{};
+		std::ifstream input;
+		input.open(s);
+		if (input)
+		{
+			std::string cadena;
+			while (input >> cadena)
+			{
+				aux.push_back(cadena);
+			}
+			return aux;
+		}
+		//Jaja, la consola no hace nada
+			//cout << "Archivo conteniendo una lista de strings no encontrado";
 		
+		return aux;
+	}
+	//Analiza un vector de ints y retorna el primer índice donde encuentre un valor específico
+	int getItemIndex(int code, std::vector<int> v)
+	{
+		if (v.size() == 0)
+		{
+			return -1;
+		}
+		for (int i = 0; i < v.size(); i++)
+		{
+			if (code == v[i])
+			{
+				return i;
+			}
+		}
+
+	}
+	//Este método hace la magia
+	void analizar(std::string inputString, std::string charset, std::string TFunc, std::string CodeList, std::string MessageList)
+	{
+		std::vector<std::vector<int>> FT{};
+		FT = cargarFT(TFunc);
+		int state = 0;
+		int symb;
+		int i = 0;
+		std::string inString;
+		std::string outputerror = "";
+		std::string outputtoken = "";
+		std::string proceso = "";
+		inString = LeerArc(inputString);
+		while (i < inString.size())
+		{
+			symb = dataType(inString.at(i), charset);
+			if (symb == -1) {
+				state = 500;
+				//cout << i << " " << symb << " " << state << " " << inString.at(i);
+			}
+			if (state < TFunc.size())
+			{
+				state = FT[state][symb];
+				//cout << i << " " << symb << " " << state << " " << inString.at(i); <--Reemplazar con equivalente en textbox
+				//Si este es el último caracter o si el siguiente estado desde el actual explota, corta el token aquí y salta a un estado
+				 //de final
+				if (i == inString.size() - 1 && state < FT.size())
+				{
+					state = FT[state][FT[state].size() - 1];
+				}
+
+			}
+
+			if (state > TFunc.size())
+			{
+				//load code list
+				std::vector<int> cList = cargarVint(CodeList);
+				//load message list
+				std::vector<std::string> mList = cargarVstring(MessageList);
+				//Get index on the message list from the code list
+				int ItemIndex = getItemIndex(state, cList);
+				if (ItemIndex == -1)
+				{
+					ItemIndex = getItemIndex(600, cList);
+				}
+
+				if(state<500)
+				{
+					outputtoken.append(mList.at(ItemIndex)+"\n");
+				}
+				else 
+				{
+					outputerror.append(mList.at(ItemIndex)+"\n");
+				}
+				//Las dejo adentro del ciclo aunque reduzcan desempeño para que se vea cómo se realiza la acción progresivamente
+				TBError->Text = convertirUM(outputerror);
+				TBToken->Text = convertirUM(outputtoken);
+
+				state = 0;
+			}
+			proceso.append(std::to_string(i) + "\t" + std::to_string(symb) + "\t" + std::to_string(state) + "\t" + inString.at(i)+"\n");
+			TBProceso->Text = convertirUM(proceso);
+			i++;
+		}
+		//Cambiar por cambio de texto en tboxes
+		//cout << "\n" << output;
+
+	}
+
 private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) 
 	{
 		
 		OpenFileDialog ofd;
 		ofd.InitialDirectory="C:";
 		ofd.Title="Abrir Archivo";
+		//ofd.Filter = "Image Files|*.jpg;*.jpeg;|All Files|*.*";
 		ofd.ShowDialog();
-		FilePath = ofd.FileName;
-		label1->Text = FilePath;
+		Input = ofd.FileName;
+		
 		
 		//Load Method
 		
@@ -265,7 +516,15 @@ private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e
 	test = convertirUM(s);
 	label1->Text = test;*/
 }
-private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) 
+{
+	std::string inputString = convertirMU(Input);
+	std::string charset = convertirMU(CharsetName);
+	std::string TFunc = convertirMU(FTName);
+	std::string CodeListFileName = convertirMU(CLName);
+	std::string MessageListFileName = convertirMU(MLName);
+	analizar(inputString,charset,TFunc,CodeListFileName,MessageListFileName);
+
 }
 private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e) {
 }
